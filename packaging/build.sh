9eb8af9f327a215a12e7b3103f4df12a34c3c73a
worldmozara <worldmozara@163.com>
2023-04-26 22:51:16 +0800

    Fix scripts

--- packaging/build.sh
+++ packaging/build.sh
@@ -6,11 +6,10 @@ VERSION="$1" # 3.8.10, 3.10.11, ...
 # ARCH="$2" # win32, amd64
 ARCH=amd64
 
-# Get minor
-VER_MINOR="${VERSION:2:2}"
-if [[ "${VER_MINOR[2]}" = "." ]]; then
-    VER_MINOR="${VERSION:2:1}"
-fi
+# Get split version number
+VER_MAJOR=$(cut -d. -f1 <<< "${VERSION}")
+VER_MINOR=$(cut -d. -f2 <<< "${VERSION}")
+VER_PATCH=$(cut -d. -f3 <<< "${VERSION}")
 
 cd ./packaging                              # pwd: PROJECT/packaging
 mkdir -p "$VERSION-$ARCH"
@@ -29,16 +28,16 @@ unzip ../vendor/tcltk.zip
 mkdir -p python
 cd python                                   # pwd: PROJECT/packaging/VERSION-ARCH/python
 unzip ../"$EPY_FN"
-rm -f ../"$EPY_FN"
-unzip ../../vendor/3."$VER_MINOR"/inject.zip
+# rm -f ../"$EPY_FN"
+unzip ../../vendor/"$VER_MAJOR.$VER_MINOR"/inject.zip
 
 ## Install Tkinter
 cp ../../vendor/{tcl,tk}86t.dll .
-cp ../../vendor/3."$VER_MINOR"/_tkinter.pyd .
-zip -mr ./python3"$VER_MINOR".zip ./tkinter
+cp ../../vendor/"$VER_MAJOR.$VER_MINOR"/_tkinter.pyd .
+zip -mr ./python"${VER_MAJOR}${VER_MINOR}".zip ./tkinter
 
 ## Install venv
 # cp python{,w}.exe ./venv-scripts/nt/
 cp python.exe venvlauncher.exe
 cp pythonw.exe venvwlauncher.exe
-zip -mr ./python3"$VER_MINOR".zip ./venv
+zip -mr ./python"${VER_MAJOR}${VER_MINOR}".zip ./venv
--- packaging/package.sh
+++ packaging/package.sh
@@ -6,11 +6,10 @@ VERSION="$1"
 # ARCH="$2"
 ARCH=amd64
 
-# Get minor
-VER_MINOR="${VERSION:2:2}"
-if [[ "${VER_MINOR[2]}" = "." ]]; then
-    VER_MINOR="${VERSION:2:1}"
-fi
+# # Get split version number
+# VER_MAJOR=$(cut -d. -f1 <<< "${VERSION}")
+# VER_MINOR=$(cut -d. -f2 <<< "${VERSION}")
+# VER_PATCH=$(cut -d. -f3 <<< "${VERSION}")
 
 cd ./packaging/"$VERSION-$ARCH"             # pwd: PROJECT/packaging/VERSION-ARCH
 zip -mr ../python-embed-plus-"$VERSION-$ARCH".zip *
